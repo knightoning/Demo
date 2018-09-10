68d90b6b4f9552edd3ee72ccedf830ef397260a6
hujie <hujie20081122@163.com>
2018-09-10 17:48:49 +0800

    add a

---
+++ src/main/java/com/knight/demo/extend/B.java
@@ -0,0 +1,16 @@
+/**
+ * Bestpay.com.cn Inc.
+ * Copyright (c) 2011-2018 All Rights Reserved.
+ */
+package com.knight.demo.extend;
+
+/**
+ * @author hujie
+ * @version Id: B.java, v 0.1 2018/8/13 17:25 hujie Exp $$
+ */
+public class B extends A{
+
+    public void init(){
+        System.out.println("-------------B---------");
+    }
+}
